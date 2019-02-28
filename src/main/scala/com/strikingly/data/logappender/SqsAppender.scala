985a94cb83f1d1486639797b6f247e927a4cd2c3
Jason Tang <jason.tang@strikingly.com>
2019-02-28 09:51:50 +0800

    remove unused code
    移除无效的、已被注释的代码

--- src/main/scala/com/strikingly/data/logappender/SqsAppender.scala
+++ src/main/scala/com/strikingly/data/logappender/SqsAppender.scala
@@ -2,8 +2,8 @@ package com.strikingly.data.logappender
 
 import java.io.Serializable
 
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder
-import org.apache.logging.log4j.core.{AbstractLifeCycle, Filter, Layout, LogEvent}
+import com.amazonaws.services.sqs.{AmazonSQS, AmazonSQSClientBuilder}
+import org.apache.logging.log4j.core.{Filter, Layout, LogEvent}
 import org.apache.logging.log4j.core.appender.AbstractAppender
 import org.apache.logging.log4j.core.config.plugins.Plugin
 import org.apache.logging.log4j.core.config.plugins.PluginAttribute
@@ -11,31 +11,12 @@ import org.apache.logging.log4j.core.config.plugins.PluginElement
 import org.apache.logging.log4j.core.config.plugins.PluginFactory
 import org.apache.logging.log4j.status.StatusLogger
 
-import scala.collection.mutable
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-
-//object SQSService {
-//  lazy val region = {
-//    val _region = System.getProperty("AWS_REGION")
-//    if (_region == null) "cn-north-1"
-//    else _region
-//  }
-//  import awscala.Region
-//  implicit lazy val sqs = SQS.at(Region(region))
-//
-//  def getQueue(name: String): Queue = {
-//    sqs.queue(name) match {
-//      case Some(queue) => queue
-//      case None =>
-//        println("create new queue")
-//        sqs.createQueueAndReturnQueueName(name)
-//    }
-//  }
-//}
+import scala.util.Try
 
 object SqsAppender {
-  lazy val sqsClient = {
+  lazy val sqsClient: AmazonSQS = {
     val region = {
       val _region = System.getProperty("AWS_REGION")
       if (_region == null) "cn-north-1"
@@ -43,14 +24,14 @@ object SqsAppender {
     }
     AmazonSQSClientBuilder.standard().withRegion(region).build()
   }
-  var sqsUrl: String = null
+  var sqsUrl: Option[String] = None
 
-  def initSqsUrl(queueName: String) = {
-    if (SqsAppender.sqsUrl == null) try
-      SqsAppender.sqsUrl = SqsAppender.sqsClient.getQueueUrl(queueName).getQueueUrl
-    catch {
-      case e: Exception =>
-        StatusLogger.getLogger.error(s"queue name error: $e")
+  def initSqsUrl(queueName: String): Unit = {
+    if (sqsUrl.isEmpty) {
+      val url = Try[String] {
+        SqsAppender.sqsClient.getQueueUrl(queueName).getQueueUrl
+      }
+      sqsUrl = Option(url.getOrElse(null))
     }
   }
 
@@ -65,12 +46,8 @@ object SqsAppender {
       return null
     }
 
-    StatusLogger.getLogger.info(s"get SQS queue name: $queueName")
+    StatusLogger.getLogger.info(s"Get SQS queue name: $queueName")
     initSqsUrl(queueName)
-
-    //    if (queue == null) {
-    //      queue = SQSService.getQueue(queueName)
-    //    }
 
     new SqsAppender(name, filter, layout, ignoreExceptions)
   }
@@ -79,28 +56,12 @@ object SqsAppender {
 @Plugin(name = "SqsAppender", category = "Core", elementType = "appender", printObject = true)
 class SqsAppender(name: String, filter: Filter, layout: Layout[_ <: Serializable],
                   ignoreExceptions: Boolean) extends AbstractAppender(name, filter, layout, ignoreExceptions) {
-  val logMsgQueue = new mutable.Queue[String]()
   override def append(event: LogEvent): Unit = {
     import com.amazonaws.services.sqs.model.SendMessageRequest
-    if (SqsAppender.sqsUrl != null) {
+
+    SqsAppender.sqsUrl.foreach { url =>
       val msg = getLayout.toSerializable(event).toString
-      Future {
-        SqsAppender.sqsClient.sendMessage(new SendMessageRequest(SqsAppender.sqsUrl, msg))
-      }
+      Future { SqsAppender.sqsClient.sendMessage(new SendMessageRequest(url, msg)) }
     }
-    //    import SQSService.sqs
-    //    if (SqsAppender.queue != null) {
-    //      logMsgQueue.enqueue(getLayout.toSerializable(event).toString)
-    //      Future {
-    //        while (logMsgQueue.nonEmpty) {
-    //          Future { SqsAppender.queue.add(logMsgQueue.dequeue()) }
-    //        }
-    //      }
-    //    }
-    //    if (SqsAppender.sqsUrl != null) {
-    //      SqsAppender.sqsClient.sendMessage(new SendMessageRequest(SqsAppender.sqsUrl, getLayout.toSerializable(event).toString))
-    //    }
   }
 }
-
-
